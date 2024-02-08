5d1bd97109fd99ac50327db6f43b2c6f9a6c85d3
ChristopherLucasBarbosaAlmeida <lucaschristofher05@gmail.com>
2024-02-08 16:32:41 -0300

    feat: add the isTextArea prop to the TextField component

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,10 +1,22 @@
 import styles from "./styles.module.scss";
 
-export function TextField() {
+type Props = {
+  label: string;
+  isTextArea?: boolean;
+};
+
+export function TextField(props: Props) {
+  const { label, isTextArea = false } = props;
+
   return (
-    <div className={styles.field}>
-      <label htmlFor="">Title</label>
-      <input type="text" placeholder="e.g. Take coffee break" />
-    </div>
+    <label htmlFor={label} className={styles.field}>
+      {label}
+
+      {isTextArea ? (
+        <textarea id={label} rows={4}></textarea>
+      ) : (
+        <input id={label} type="text" placeholder="e.g. Take coffee break" />
+      )}
+    </label>
   );
 }
