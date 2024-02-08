b2146221b10011da78c6aba985ebc5400df55beb
ChristopherLucasBarbosaAlmeida <lucaschristofher05@gmail.com>
2024-02-08 16:31:36 -0300

    feat: add Button component variants

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,13 +4,22 @@ import styles from "./styles.module.scss";
 interface Props {
   children: ReactNode;
   onClick?: () => void;
+  variant: keyof typeof variants;
 }
 
-export  function Button(props: Props) {
-  const { children, onClick } = props;
+const variants = {
+  primary: styles.button__primary,
+  secondary: styles.button__secondary,
+};
+
+export function Button(props: Props) {
+  const { children, onClick, variant } = props;
 
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button
+      className={`${styles.button} ${variants[variant]}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
