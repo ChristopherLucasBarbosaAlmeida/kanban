e4f229249695f46aa4201223355d01e6ff6b88b3
ChristopherLucasBarbosaAlmeida <lucaschristofher05@gmail.com>
2024-02-21 20:20:25 -0300

    feat: add CreateNewColumnModal component

--- src/components/CreateNewTaskModal/index.tsx
+++ src/components/CreateNewTaskModal/index.tsx
@@ -1,23 +1,39 @@
 import { useContext, useState } from "react";
 import { Button, Modal, SelectField, TextField } from "..";
 import { KanbanContext } from "../../context/Kanban";
+import { X } from "lucide-react";
+import styles from "./styles.module.scss";
+import { Column } from "../../types/Column";
+import { Subtask } from "../../types/Subtask";
+import { Task } from "../../types/Task";
 
 type CreateNewTaskModalProps = {
+  boardId: string;
+  columns: Column[];
   showCreateNewTaskModal: boolean;
   handleClickModalBackground: () => void;
 };
 
 export function CreateNewTaskModal(props: CreateNewTaskModalProps) {
-  const { showCreateNewTaskModal, handleClickModalBackground } = props;
+  const {
+    showCreateNewTaskModal,
+    handleClickModalBackground,
+    boardId,
+    columns,
+  } = props;
   const { kanban, setKanban } = useContext(KanbanContext);
-
   const [title, setTitle] = useState("");
+  const [subtaskDescription, setSubtaskDescription] = useState("");
+  const [subtasks, setSubtasks] = useState<Subtask[]>([]);
+  const [selectedColumnId, setSeletedColumnId] = useState("Todo");
+  const [description, setDescription] = useState("");
 
   function handleCreateNewTask() {
+    if (!title) return;
     const updated = kanban.map((k) => {
-      if (k.id === "B1") {
-        k.columns = k.columns.map((column) => {
-          if (column.name === "Testing") {
+      if (k.id === boardId) {
+        const newColumns = k.columns.map((column) => {
+          if (column.id === selectedColumnId) {
             return {
               ...column,
               tasks: [
@@ -25,17 +41,58 @@ export function CreateNewTaskModal(props: CreateNewTaskModalProps) {
                 {
                   id: window.crypto.randomUUID(),
                   title,
-                  subtasks: [],
-                },
+                  description,
+                  subtasks,
+                } as Task,
               ],
-            };
+            } as Column;
           }
           return column;
         });
+        return { ...k, columns: newColumns };
       }
       return k;
     });
     setKanban(updated);
+    setTitle("");
+    setDescription("");
+    setSubtasks([]);
+  }
+
+  function handleCreateSubtasks() {
+    if (!subtaskDescription) {
+      return;
+    }
+    setSubtasks((prev) => [
+      ...prev,
+      {
+        id: window.crypto.randomUUID(),
+        done: false,
+        description: subtaskDescription,
+      },
+    ]);
+    setSubtaskDescription("");
+  }
+
+  function handleDeleteSubtask(subtaskId: string) {
+    setSubtasks(subtasks.filter((subtask) => subtask.id !== subtaskId));
+  }
+
+  function handleUpdatedSubtaskDescription(
+    description: string,
+    subtaskId: string
+  ) {
+    setSubtasks(
+      subtasks.map((subtask) => {
+        if (subtask.id === subtaskId) {
+          return {
+            ...subtask,
+            description,
+          };
+        }
+        return subtask;
+      })
+    );
   }
 
   return (
@@ -44,15 +101,43 @@ export function CreateNewTaskModal(props: CreateNewTaskModalProps) {
       handleClickModalBackground={handleClickModalBackground}
     >
       <strong>Add New Task</strong>
-      <TextField label="Title" onChange={(ev) => setTitle(ev.target.value)} />
+      <TextField
+        label="Title"
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
       <TextField
         label="Description"
         isTextArea
-        // onChange={(ev) => setDescription(ev.target.value)}
+        onChange={(ev) => setDescription(ev.target.value)}
       />
-      <TextField label="Subtasks" />
-      <Button variant="secondary">Add New Subtask</Button>
-      <SelectField label="Status" onChange={(ev) => console.log(ev?.option)} />
+      <div className={styles.container__subtasks}>
+        <TextField
+          label="Subtasks"
+          value={subtaskDescription}
+          onChange={(ev) => setSubtaskDescription(ev.target.value)}
+        />
+        {subtasks.map((subtask) => (
+          <div>
+            <TextField
+              value={subtask.description}
+              key={subtask.id}
+              onChange={(ev) =>
+                handleUpdatedSubtaskDescription(ev.target.value, subtask.id)
+              }
+            />
+            <X onClick={() => handleDeleteSubtask(subtask.id)} />
+          </div>
+        ))}
+        <Button variant="secondary" onClick={handleCreateSubtasks}>
+          Add New Subtask
+        </Button>
+      </div>
+      <SelectField
+        label="Status"
+        onChange={(ev) => setSeletedColumnId(ev!.value)}
+        columns={columns}
+      />
       <Button variant="primary" onClick={handleCreateNewTask}>
         Create Task
       </Button>
