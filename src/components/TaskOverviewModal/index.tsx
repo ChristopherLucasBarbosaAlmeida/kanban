1f68c0d9c84c00877828164a8e20ee5f243bab09
ChristopherLucasBarbosaAlmeida <lucaschristofher05@gmail.com>
2024-02-21 20:15:40 -0300

    feat: add feature to create new task

--- src/components/TaskOverviewModal/index.tsx
+++ src/components/TaskOverviewModal/index.tsx
@@ -1,28 +1,42 @@
-import { Modal, SelectField, Subtask } from "..";
+import { Modal, Subtask } from "..";
+import { Task } from "../../types/Task";
 import styles from "./styles.module.scss";
 
-export function TaskOverviewModal() {
+type Props = {
+  taskOverViewModalIsOpen: boolean;
+  handleClickBackGroundeModal: () => void;
+  task?: Task;
+};
+
+export function TaskOverviewModal(props: Props) {
+  const { taskOverViewModalIsOpen, handleClickBackGroundeModal, task } = props;
+
+  const subtasks = task?.subtasks.length;
+  const subtaskCompleted = task?.subtasks.filter(
+    (subtask) => subtask.done
+  ).length;
+
   return (
-    <Modal>
-      <strong>
-        Research pricing points of valours competitors and trial different
-        business models
-      </strong>
-      <p>
-        We know we re planning to build for version one. Now we need to finalise
-        the first pricing model well use. Keep iterating the subtasks until we
-        have a coherente proposition
-      </p>
-      <div className={styles.container__subtasks}>
-        <span>Subtasks (2 of 3)</span>
-        <Subtask>Research competitor pricing adn business models</Subtask>
-        <Subtask>Outline a business model that works for our solution</Subtask>
-        <Subtask>
-          Talk to pontential customers about our proposet solution and ask for
-          fair price expectancy
-        </Subtask>
-      </div>
-      <SelectField label="Status" />
+    <Modal
+      isShow={taskOverViewModalIsOpen}
+      handleClickModalBackground={handleClickBackGroundeModal}
+    >
+      {!task ? (
+        <span>Task not found</span>
+      ) : (
+        <>
+          <strong>{task.title}</strong>
+          <p>{task.description}</p>
+          <div className={styles.container__subtasks}>
+            <span>
+              Subtasks ({subtaskCompleted} of {subtasks})
+            </span>
+            {task.subtasks.map((subtask) => (
+              <Subtask key={subtask.id}>{subtask.description}</Subtask>
+            ))}
+          </div>
+        </>
+      )}
     </Modal>
   );
 }
